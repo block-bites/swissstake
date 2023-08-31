4ea6ff1f33f74ce0856ce5973266ec1f420df7f0
Jan Podleski <jpodly.priv@gmail.com>
2023-08-31 13:46:44 +0200

    v7

--- src/components/molecules/infrastructure-block.js
+++ src/components/molecules/infrastructure-block.js
@@ -2,12 +2,13 @@ import React from "react";
 
 function InfrastructureBlock({title, text, image}) {
   return (
-    <div className="infrastrucutre-block">
-      <h4 className="infrastrucutre-block__title">{title}</h4>
-      <p className="infrastrucutre-block__text">
+    <div className="infrastructure-block">
+      <h4 className="infrastructure-block__title">{title}</h4>
+      <p className="infrastructure-block__text">
        {text}
       </p>
-      <img src={image} alt="" className="infrastrucutre-block__img"/>
+      
+      <img src={image} alt="" className="infrastructure-block__img"/>
     </div>
   );
 }
--- src/components/organisms/infrastructure.js
+++ src/components/organisms/infrastructure.js
@@ -5,17 +5,20 @@ import InfrastructureBlock from '../molecules/infrastructure-block'
 
 import PrivacyImage from "../../assets/infrastructureMap.svg"
 
-
 function Infrastructure() {
   return (
     <div className='infrastructure'> 
-        {/* <h2 className='infrastructure__title'>Swiss Infrastructure</h2>
-        <h4>
+        <h2 className='infrastructure__title'>Swiss Infrastructure</h2>
+        <h4 className="infrastructure__subtitle">
           Our server is proudly hosted in Basel, Switzerland, leveraging the
           robust Swiss infrastructure.
         </h4>
-        <p> This location offers:</p> */}
+        <p> This location offers:</p>
+      <InfrastructureBlock title={"Stability"} text="sdsdsadasdsdasdasdsadsadsaddasdsa" image={PrivacyImage}/>
+        <div className='infrastructure__blocks-in-row'>
       <InfrastructureBlock title={"Privacy"} text="sdsdsadasdasdsa" image={PrivacyImage}/>
+      <InfrastructureBlock title={"Quality"} text="sdsdsadasdasdsadsadsadasdsa" image={PrivacyImage}/>
+      </div>
     </div>
   )
 }
