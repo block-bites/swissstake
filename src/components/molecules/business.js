a997692a5f6e17340138e47e08112ab9adfc4f3d
Jan Podleski <jpodly.priv@gmail.com>
2023-09-02 21:09:54 +0200

    v9

--- src/components/molecules/business.js
+++ src/components/molecules/business.js
@@ -3,19 +3,21 @@ import Frame4 from "../../assets/LogoCompany.svg";
 
 function Business() {
   return (
-    <div className="business">
-    <div className="businessText">
-    <p>Businesses already run their site on SwissStake</p>
-    </div>
-      <div className="businessLogo">
-      <img src={Frame4} alt="Businesses on SwissStake" />
-      <img src={Frame4} alt="Businesses on SwissStake" />
-      <img src={Frame4} alt="Businesses on SwissStake" />
-      <img src={Frame4} alt="Businesses on SwissStake" />
-      <img src={Frame4} alt="Businesses on SwissStake" />
-      <img src={Frame4} alt="Businesses on SwissStake" />
-      <img src={Frame4} alt="Businesses on SwissStake" />
-      <img src={Frame4} alt="Businesses on SwissStake" />
+    <div className="business-positioner">
+      <div className="business">
+        <div className="business__text">
+          <p>Businesses already run their site on SwissStake</p>
+        </div>
+        <div className="business__logo">
+          <img src={Frame4} alt="Businesses on SwissStake" />
+          <img src={Frame4} alt="Businesses on SwissStake" />
+          <img src={Frame4} alt="Businesses on SwissStake" />
+          <img src={Frame4} alt="Businesses on SwissStake" />
+          <img src={Frame4} alt="Businesses on SwissStake" />
+          <img src={Frame4} alt="Businesses on SwissStake" />
+          <img src={Frame4} alt="Businesses on SwissStake" />
+          <img src={Frame4} alt="Businesses on SwissStake" />
+        </div>
       </div>
     </div>
   );
--- src/components/molecules/why-choose.js
+++ src/components/molecules/why-choose.js
@@ -1,21 +1,26 @@
 import React from "react";
+
 import Frame2 from "../../assets/Frame 2.svg";
 
 function Whychoose() {
   return (
-    <div className="whychoose">
-      <div className="whychooseBlock">
-        <div className="whychooseText">
-          <h2>Why Choose SwissStake?</h2>
-          <p>
-            SwissStake is not just another validator; we offer unique advantages
-            that set us apart <br /> from the crowd. Our commitment to
-            independence, quality, and experience makes us the <br /> ideal
-            choice for staking on the Casper Network.
-          </p>
+    <div className="whychoose-positioner">
+      <div className="whychoose">
+        <div className="whychoose__block">
+          <div className="whychoose__text">
+            <h2>Why Choose SwissStake?</h2>
+            <p>
+              SwissStake is not just another validator; we offer unique
+              advantages that set us apart <br /> from the crowd. Our commitment
+              to independence, quality, and experience makes us the <br /> ideal
+              choice for staking on the Casper Network.
+            </p>
+          </div>
+          <div className="whychoose__casper">
+            <img src={Frame2} alt="casper ecosystem img" />
+          </div>
         </div>
       </div>
-      <img className="whychoose__casper" src={Frame2} alt="casper ecosystem img" />
     </div>
   );
 }
