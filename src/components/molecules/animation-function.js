ff07952e86889305c21be66623da07d2a224f6de
Budyn0 <jpodly.priv@gmail.com>
2023-10-20 01:00:11 +0200

    1. Instalacja react-scroll

--- src/components/molecules/animation-function.js
+++ src/components/molecules/animation-function.js
@@ -1,13 +1,30 @@
 import React from "react";
+import { Parallax, ParallaxProvider } from "react-scroll-parallax";
 
-import Component5 from "../../assets/Component 5.svg";
+import First from "../../assets/Animation-first.svg";
+import Second from "../../assets/Animation-second.svg";
+import Third from "../../assets/Animation-third.svg";
+import Fourth from "../../assets/Animation-fourth.svg";
 
-function Animation() {
+const ImageAnimation = () => {
   return (
-    <div className="animation">
-      <img className="animation__img" src={Component5} alt="animation" />
-    </div>
+    <ParallaxProvider>
+      <div className="animation-container">
+        <Parallax className="parallax-image" x={[-20, 20]}>
+          <img src={First} alt="Image 1" />
+        </Parallax>
+        <Parallax className="parallax-image" x={[-40, 40]}>
+          <img src={Second} alt="Image 2" />
+        </Parallax>
+        <Parallax className="parallax-image" x={[-60, 60]}>
+          <img src={Third} alt="Image 3" />
+        </Parallax>
+        <Parallax className="parallax-image" x={[-80, 80]}>
+          <img src={Fourth} alt="Image 4" />
+        </Parallax>
+      </div>
+    </ParallaxProvider>
   );
-}
+};
 
-export default Animation;
+export default ImageAnimation;
