3c711a6e27f21b59f8ee301e86a1e13d9986e19d
Budyn <jpodly.priv@gmail.com>
2023-11-20 14:08:21 +0100

    1. Dodanie zdjęć

--- src/components/organisms/animation.js
+++ src/components/organisms/animation.js
@@ -1,11 +1,19 @@
 import React from "react";
+import useIsMobile from "../../hooks/useIsMobile";
 
-import Animation from "../../assets/Fourth-animation.png";
+import Animation from "../../assets/Animation.png";
+import MobileAnimation from "../../assets/Mobile-Animation.png";
 
 function ImageScroller() {
+  const isMobile = useIsMobile();
+
   return (
     <div className="outer-container" id="about">
-      <img src={Animation} alt="animation" className="image" />
+      {isMobile ? (
+        <img src={MobileAnimation} alt="animation" className="image"></img>
+      ) : (
+        <img src={Animation} alt="animation" className="image"></img>
+      )}
     </div>
   );
 }
