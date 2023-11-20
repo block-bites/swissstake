7e1a79572ddd995221c8ca7c71a5f81231b421ce
Budyn <jpodly.priv@gmail.com>
2023-11-20 15:38:16 +0100

    1. Validator zmiana formatowania tekstu

--- src/components/molecules/validator.js
+++ src/components/molecules/validator.js
@@ -8,33 +8,14 @@ function Validator() {
       <div className="validator">
         <h1 className="validator__desktop-title">
           <span className="validator__desktop-title--blue">SwissStake</span> -
-          Your Swiss Validator <br />
-          on <br /> the Casper Network
-        </h1>
-
-        <h1 className="validator__mobile-title">
-          <span className="validator__mobile-title--blue">SwissStake</span> -<br />
-          Your Swiss<br /> Validator on <br />the Casper<br /> Network
+          Your Swiss Validator on <br /> the Casper Network
         </h1>
 
         <div className="validator__subtitle">
           <p className="validator__subtitle">
             Start staking with SwissStake and experience the difference that our
-            unique <br /> advantages bring. Whether you're new to staking or a
-            seasoned pro, we're here to <br /> support you every step of the
-            way.
-          </p>
-        </div>
-
-        <div className="validator__mobile-subtitle">
-          <p className="validator__mobile-subtitle">
-            Start staking with SwissStake and
-            <br /> experience the difference that our
-            <br />
-            unique advantages bring. Whether
-            <br /> you're new to staking or a<br />
-            seasoned pro, we're here to <br /> support you every step of the
-            way.
+            unique advantages bring. Whether you're new to staking or a seasoned
+            pro, we're here to support you every step of the way.
           </p>
         </div>
 
