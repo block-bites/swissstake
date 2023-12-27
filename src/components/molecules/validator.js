41cf22ae2263dc105cf0e3fd94c0cfa16001b10c
Budyn <jpodly.priv@gmail.com>
2023-12-27 07:51:53 +0100

    1. Intro

--- src/components/molecules/validator.js
+++ src/components/molecules/validator.js
@@ -2,30 +2,26 @@ import React from "react";
 
 import Primarybtn from "../atoms/primary-btn";
 
-
 function Validator() {
   return (
     <div className="validator-positioner">
       <div className="validator">
-        <div>
+        <div className="validator__content">
           <h1 className="validator__title">SwissStake</h1>
-        </div>
-        <div>
+
           <h2 className="validator__subtitle">
             Your Swiss Validator on the Casper Network
           </h2>
-        </div>
 
-        <div className="validator__text">
           <p className="validator__text">
             Start staking with SwissStake and experience the difference that our
             unique advantages bring. Whether you're new to staking or a seasoned
             pro, we're here to support you every step of the way.
           </p>
-        </div>
 
-        <div className="validator__button">
-          <Primarybtn />
+          <div className="validator__button">
+            <Primarybtn />
+          </div>
         </div>
       </div>
     </div>
