8061c5f2d6e6469a8fd43ab7d0ca361887957e83
Budyn <jpodly.priv@gmail.com>
2024-01-22 15:31:14 +0100

    1. cleaning code

--- src/components/atoms/primary-btn.js
+++ src/components/atoms/primary-btn.js
@@ -2,16 +2,14 @@ import React from "react";
 
 function Primarybtn() {
   return (
-    <div>
-      <a
-        className="primary-btn"
-        href="https://cspr.live/validator/01ebaebffebe63ee6e35b88697dd9d5bfab23dac47cbd61a45efc8ea8d80ec9c38"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Get Started
-      </a>
-    </div>
+    <a
+      className="primary-btn"
+      href="https://cspr.live/validator/01ebaebffebe63ee6e35b88697dd9d5bfab23dac47cbd61a45efc8ea8d80ec9c38"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Get Started
+    </a>
   );
 }
 
