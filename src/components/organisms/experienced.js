eb554065130ee20d74bc1dcc07afa57c1d595419
Budyn <jpodly.priv@gmail.com>
2024-01-25 14:27:28 +0100

    1. experienced component optimization

--- src/components/organisms/experienced.js
+++ src/components/organisms/experienced.js
@@ -1,17 +1,14 @@
 import React from "react";
-import useIsMobile from "../../hooks/useIsMobile";
 
 import ExperiencedBlock from "../molecules/experienced-block";
-import MobileExperiencedBlock from "../molecules/mobile-experienced-block";
 import Joinblock from "../molecules/join-block";
 
 function Experienced() {
-  const isMobile = useIsMobile();
 
   return (
     <div className="experienced-positioner" id="adopter">
       <div className="experienced-block">
-        {isMobile ? <MobileExperiencedBlock /> : <ExperiencedBlock />}
+        <ExperiencedBlock />
       </div>
       <div className="experienced-join">
         <Joinblock />
