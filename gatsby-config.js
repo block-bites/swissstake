4f12b407629839200d05d60d7243d48055bd0f58
wojtek-blockbites <wojtek@blockbit.es>
2024-02-05 13:26:14 +0100

    gtm usage fixed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
     title: `Swissstake`,
